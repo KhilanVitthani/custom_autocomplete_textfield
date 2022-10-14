6d0bcc58403b529f94400f0dfa6acbd8fed753ec
KhilanVitthani <khilanvithani0080@gmail.com>
2022-10-14 15:55:18 +0530

    initial commit

--- lib/main.dart
+++ lib/main.dart
@@ -8,6 +8,22 @@ class MyApp extends StatefulWidget {
 }
 
 class _MyAppState extends State<MyApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: AutoCompleteTextField(),
+    );
+  }
+}
+
+class AutoCompleteTextField extends StatefulWidget {
+  const AutoCompleteTextField({Key? key}) : super(key: key);
+
+  @override
+  State<AutoCompleteTextField> createState() => _AutoCompleteTextFieldState();
+}
+
+class _AutoCompleteTextFieldState extends State<AutoCompleteTextField> {
   bool isChanged = false;
   List<Continent> continentOptions = <Continent>[
     Continent(name: 'africa', size: 30370000),
@@ -51,67 +67,63 @@ class _MyAppState extends State<MyApp> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: SafeArea(
-        child: Scaffold(
-          body: SingleChildScrollView(
-            child: Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: Column(
-                children: [
-                  //  AutoCompleteExample(),
-                  (continent.isNotEmpty)
-                      ? Column(
-                          children: List.generate(continent.length, (index) {
-                          return ListTile(
-                            title: Text(continent[index]),
-                            onTap: () {
-                              setState(() {
-                                serchController.text = continent[index];
-                                continent.clear();
-                              });
-                            },
-                          );
-                        }))
-                      : SizedBox(),
+    return SafeArea(
+      child: Scaffold(
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                //  AutoCompleteExample(),
+                (continent.isNotEmpty)
+                    ? Column(
+                        children: List.generate(continent.length, (index) {
+                        return ListTile(
+                          title: Text(continent[index]),
+                          onTap: () {
+                            setState(() {
+                              serchController.text = continent[index];
+                              continent.clear();
+                            });
+                          },
+                        );
+                      }))
+                    : SizedBox(),
 
-                  TextField(
-                    controller: serchController,
-                    onChanged: (val) {
-                      if (val.isNotEmpty) {
-                        setState(() {
-                          isChanged = true;
-                        });
+                TextField(
+                  controller: serchController,
+                  onChanged: (val) {
+                    if (val.isNotEmpty) {
+                      setState(() {
+                        isChanged = true;
+                      });
+                      continent.clear();
+                      continentOptions.forEach((element) {
+                        if (element.name[0] == val.toLowerCase()) {
+                          continent.add(element.name.toString());
+                          print(continent);
+                        }
+                        if (element.name.substring(0, 2) == val.toLowerCase()) {
+                          // continent.clear();
+                          continent.add(element.name.toString());
+                          print(continent);
+                        }
+                        if (element.name.substring(0, 3) == val.toLowerCase()) {
+                          // continent.clear();
+                          continent.add(element.name.toString());
+                          print(continent);
+                        }
+                      });
+                      print(isChanged);
+                    } else {
+                      setState(() {
                         continent.clear();
-                        continentOptions.forEach((element) {
-                          if (element.name[0] == val.toLowerCase()) {
-                            continent.add(element.name.toString());
-                            print(continent);
-                          }
-                          if (element.name.substring(0, 2) ==
-                              val.toLowerCase()) {
-                            // continent.clear();
-                            continent.add(element.name.toString());
-                            print(continent);
-                          }
-                          if (element.name.substring(0, 3) ==
-                              val.toLowerCase()) {
-                            // continent.clear();
-                            continent.add(element.name.toString());
-                            print(continent);
-                          }
-                        });
-                        print(isChanged);
-                      } else {
-                        setState(() {
-                          continent.clear();
-                          isChanged = false;
-                        });
-                      }
-                    },
-                  ),
-                ],
-              ),
+                        isChanged = false;
+                      });
+                    }
+                  },
+                ),
+              ],
             ),
           ),
         ),
@@ -134,74 +146,3 @@ class Continent {
     return '$name ($size)';
   }
 }
-
-//
-// class AutoCompleteExample extends StatefulWidget {
-//   @override
-//   State<StatefulWidget> createState() => _AutoCompleteExampleState();
-// }
-//
-// class _AutoCompleteExampleState extends State<AutoCompleteExample> {
-//   @override
-//   Widget build(BuildContext context) {
-//     return Padding(
-//       padding: EdgeInsets.all(15.0),
-//       child: Container(
-//         alignment: Alignment.bottomCenter,
-//         child: Autocomplete<Continent>(
-//           fieldViewBuilder: (BuildContext context,
-//               TextEditingController fieldTextEditingController,
-//               FocusNode fieldFocusNode,
-//               VoidCallback onFieldSubmitted) {
-//             return TextField(
-//               controller: fieldTextEditingController,
-//               focusNode: fieldFocusNode,
-//               style: const TextStyle(fontWeight: FontWeight.bold),
-//             );
-//           },
-//           optionsBuilder: (TextEditingValue textEditingValue) {
-//             return continentOptions
-//                 .where((Continent continent) => continent.name
-//                     .toLowerCase()
-//                     .startsWith(textEditingValue.text.toLowerCase()))
-//                 .toList();
-//           },
-//           displayStringForOption: (Continent option) => option.name,
-//           onSelected: (Continent selection) {
-//             print('Selected: ${selection.name}');
-//           },
-//           optionsViewBuilder: (BuildContext context,
-//               AutocompleteOnSelected<Continent> onSelected,
-//               Iterable<Continent> options) {
-//             return Align(
-//               alignment: Alignment.topLeft,
-//               child: Material(
-//                 child: Container(
-//                   width: 300,
-//                   color: Colors.teal,
-//                   child: ListView.builder(
-//                     padding: EdgeInsets.all(10.0),
-//                     itemCount: options.length,
-//                     itemBuilder: (BuildContext context, int index) {
-//                       final Continent option = options.elementAt(index);
-//
-//                       return GestureDetector(
-//                         onTap: () {
-//                           onSelected(option);
-//                         },
-//                         child: ListTile(
-//                           title: Text(option.name,
-//                               style: const TextStyle(color: Colors.white)),
-//                         ),
-//                       );
-//                     },
-//                   ),
-//                 ),
-//               ),
-//             );
-//           },
-//         ),
-//       ),
-//     );
-//   }
-// }
